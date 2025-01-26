e5853baac4b5e7b78b2fbad9b55001ce647372fc
Marcel Köberlin <marcel.koeberlin@posteo.ch>
2025-01-26 17:14:59 +0100

    Update site look

--- src/helpers/userUtils.js
+++ src/helpers/userUtils.js
@@ -1,7 +1,23 @@
-// Put your computations here.
-
-function userComputed(data) {
-  return {};
+function shuffle(a) {
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = a[i];
+    a[i] = a[j];
+    a[j] = x;
+  }
+  return a;
 }
 
-exports.userComputed = userComputed;
+function sliceIntoChunks(arr, chunkSize) {
+  const res = [];
+  for (let i = 0; i < arr.length; i += chunkSize) {
+    const chunk = arr.slice(i, i + chunkSize);
+    res.push(chunk);
+  }
+  return res;
+}
+
+// Export only necessary functions
+exports.shuffle = shuffle;
+exports.sliceIntoChunks = sliceIntoChunks;
--- src/site/_data/eleventyComputed.js
+++ src/site/_data/eleventyComputed.js
@@ -1,9 +1,7 @@
 const { getGraph } = require("../../helpers/linkUtils");
 const { getFileTree } = require("../../helpers/filetreeUtils");
-const { userComputed } = require("../../helpers/userUtils");
 
 module.exports = {
   graph: (data) => getGraph(data),
   filetree: (data) => getFileTree(data),
-  userComputed: (data) => userComputed(data)
 };
